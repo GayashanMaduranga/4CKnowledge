0b22819a1cd04d3e730781c7de15a4f64d0e1ce5
gayashan kariyawasam <gayashan.kariyawasam@my.sliit.lk>
2018-03-27 14:41:48 +0530

    adds member entity

--- src/com/sliit/datamodel/DBConnection.java
+++ src/com/sliit/datamodel/DBConnection.java
@@ -1,4 +1,4 @@
-package com.sliit.dao;
+package com.sliit.datamodel;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
---
+++ src/com/sliit/entities/Member.java
@@ -0,0 +1,90 @@
+package com.sliit.entities;
+
+import java.sql.Date;
+
+public class Member {
+	
+	private int id;
+	
+	private String email;
+	private String fname;
+	private String lname;
+	private String password;
+	private String address;
+	private String mobile;
+	private String homeTel;
+	private Date dob;
+	private int userLevel;
+
+	
+		
+	public Member() {
+		
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getHomeTel() {
+		return homeTel;
+	}
+	public void setHomeTel(String homeTel) {
+		this.homeTel = homeTel;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	public int getUserLevel() {
+		return userLevel;
+	}
+	public void setUserLevel(int userLevel) {
+		this.userLevel = userLevel;
+	}
+	
+	
+
+	
+	
+
+}
