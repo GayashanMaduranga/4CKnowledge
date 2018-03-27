d98e2b8e2541eca62f2a897ff1d34896c5680806
gayashan kariyawasam <gayashan.kariyawasam@my.sliit.lk>
2018-03-27 13:26:57 +0530

    adds DBConnection class

---
+++ src/com/sliit/dao/DBConnection.java
@@ -0,0 +1,38 @@
+package com.sliit.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+	public static Connection getConnectionToDatabase() {
+		Connection connection = null;
+
+		try {
+
+			
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("MySQL JDBC Driver Registered!");
+
+			//Created this class temporally 
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/4CKnowledge", "root", "\\&4+S:3<!'0!OsK");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Where is your MySQL JDBC Driver?");
+			e.printStackTrace();
+
+		}
+
+		catch (SQLException e) {
+			System.out.println("Connection Failed! Check output console");
+			e.printStackTrace();
+
+		}
+
+		if (connection != null) {
+			System.out.println("Connection made to DB!");
+		}
+		return connection;
+	}
+
+}
